9676a768753e4d84fff7726f57fa6113a41ed745
Joel <95248566+joelamponsah@users.noreply.github.com>
2025-09-10 12:50:56 +0000

    new file to train models

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-#%pip install streamlit
+%pip install streamlit
 
 #from google.colab import drive
 #drive.mount('/content/drive')
